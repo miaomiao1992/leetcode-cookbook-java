b523f5df7133fa165e574f7554e4ef6faa59dfd4
miaomiao1992 <644284807@qq.com>
2022-08-30 09:59:14 +0800

    2022-08-30-09:59-push

--- git-push.bat
+++ git-push.bat
@@ -4,13 +4,15 @@ set "Time=%time:~0,5%"
 set "t=%Time: =0%"
 ::echo %t%
 
-set "Ymd=%date:~,4%-%date:~5,2%-%date:~8,2%-%t%-push"
+set "remarks=%date:~,4%-%date:~5,2%-%date:~8,2%-%t%-push"
+
+echo %remarks%
 
  git add src
  git add git-cookbook
  git add  README.md LICENSE
  git add git-push.bat 
- git commit -m  %Ymd%
+ git commit -m  %remarks%
  git push origin main
 
 echo git-push success!

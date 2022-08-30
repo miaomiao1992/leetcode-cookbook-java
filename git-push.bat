87a7b13a31bfbbbc475ea9fd6da32f51b500eeb2
miaomiao1992 <644284807@qq.com>
2022-08-30 17:54:49 +0800

    2022-08-30-17:54:48-push-miaomiao1992

--- git-push.bat
+++ git-push.bat
@@ -1,19 +1,21 @@
 @echo off
 
+::获取系统时间
 set "Time=%time:~0,8%"
 set "t=%Time: =0%"
-::echo %t%
 
-set "remarks=%date:~,4%-%date:~5,2%-%date:~8,2%-%t%-push"
+set "remarks=%date:~,4%-%date:~5,2%-%date:~8,2%-%t%-push-miaomiao1992"
 echo %remarks%
  
 
- git add src
- git add git-cookbook
- git add  README.md LICENSE
- git add git-push.bat 
- git commit -m  %remarks%
- git push origin main
+::添加需要上传的项目
+git add src
+git add git-cookbook
+git add  README.md LICENSE
+git add git-push.bat
+
+git commit -m  %remarks%
+git push origin main
 
 echo git-push success!
 
